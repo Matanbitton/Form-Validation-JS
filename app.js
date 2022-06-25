3af148e3224496759218c44f80f76bbac224b9fc
Matan <matanbitton2@gmail.com>
2022-06-25 15:05:26 +0300

    add password validation

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const passwordValidateInput = document.getElementById("password2");
 const emailPattern = "/S+@S+.S+/";
 const countryPattern = "[a-zA-Z'-'s]*";
 const zipCodePattern = "[0-9]{1,5}";
+const passwordPattern =
+  "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$";
 
 zipCodeInput.setAttribute("minlength", "4");
 zipCodeInput.setAttribute("pattern", zipCodePattern);
 countryInput.setAttribute("pattern", countryPattern);
+passwordInput.setAttribute("pattern", passwordPattern);
+passwordInput.setAttribute("minlength", "8");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -60,19 +64,29 @@ countryInput.addEventListener("input", () => {
   }
 });
 
+passwordInput.addEventListener("input", () => {
+  passwordValidateInput.setAttribute("pattern", passwordInput.value);
+  if (passwordInput.validity.patternMismatch) {
+    passwordInput.setCustomValidity(
+      "Password must be 8 characters long. Contain 1 Uppercase letter, 1 Lowercase letter, a number, and a symbol."
+    );
+    passwordInput.reportValidity();
+  } else {
+    passwordInput.setCustomValidity("");
+    passwordInput.checkValidity();
+  }
+});
+
+passwordValidateInput.addEventListener("input", () => {
+  if (passwordValidateInput.validity.patternMismatch) {
+    passwordValidateInput.setCustomValidity("Please make sure passwords match");
+    passwordValidateInput.reportValidity();
+  } else {
+    passwordValidateInput.setCustomValidity("");
+    passwordValidateInput.checkValidity();
+  }
+});
+
 const validateEmail = (email) => {
   return email.match(emailPattern);
 };
-
-function setErrorMessagesOnInputs(formInput, errorMessage) {
-  formInput.addEventListener("input", () => {
-    formInput.setCustomValidity("");
-    formInput.checkValidity();
-  });
-
-  formInput.addEventListener("invalid", () => {
-    if (formInput.value === "") {
-      formInput.setCustomValidity(errorMessage);
-    }
-  });
-}
